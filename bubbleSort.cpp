4c3c4669b0511b9c1b9ae34149514d7fa6fd323e
Sumukha BS <77795919+Sumukha11@users.noreply.github.com>
2025-03-10 13:10:46 +0530

    Adding BubbleSort

---
+++ bubbleSort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    //Bubble sort
+
+    for(int i=n-1;i>0;i--){
+        for(int j=0;j<i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
